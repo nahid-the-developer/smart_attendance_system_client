09c09fcd826ac392a7dda160800ba1f3f487f9fe
nahid-the-developer <nahidcse.pub@gmail.com>
2024-04-27 21:02:02 +0600

    fix conflict

--- src/providers/themeProvider.js
+++ src/providers/themeProvider.js
@@ -4,6 +4,7 @@ import {ThemeProvider as MuiThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import {createTheme} from "@mui/material";
 import Navbar from "@/components/Navbar";
+import {useTheme} from "next-themes";
 
 const lightTheme = createTheme({
     palette: {
@@ -18,12 +19,22 @@ const darkTheme = createTheme({
 });
 
 export default function ThemeProvider({children}) {
+    const [mounted, setMounted] = useState(false)
+    const {theme, setTheme} = useTheme()
 
     useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
     }
 
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
     return (
+        <MuiThemeProvider theme={themeMode}>
             <CssBaseline/>
+            <Navbar toggleThemeMode={() => setTheme(theme === 'dark' ? 'light' : 'dark')} darkMode={theme === 'dark'}/>
             {children}
         </MuiThemeProvider>
     );
